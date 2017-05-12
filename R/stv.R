0cd621218f56871d495e3d5662f57dac326cddfb
JWE <jay@JWEs-MacBook-Pro.local>
2017-05-12 09:16:16 -0400

    First commit

---
+++ R/stv.R
@@ -0,0 +1,141 @@
+#' Implement STV counting system.
+#'
+#' \code{stv} returns a data.frame with rows containing detailed results
+#' from each round of STV counting.
+#'
+#' \code{stv()} first validates \code{x} by running \code{validateBallots()} function.
+#' Once validation is complete, it implements Single Transferable Vote
+#' counting method. Each round of counting starts with idetification of active
+#' ballots. Then quota is calculated (currently only supports Droop method \code{INCLUDE FORMULA?}).
+#' Tally of candidates is obtained using top choices of active ballots. If a
+#' candidate reaches quota, she/he is elected and associated surplus ballots
+#' are reallocated (currently only supports Cambridge method \code{ONE LINE DETAIL?}). If multiple candidates
+#' reach quota, all of them are elected and their surplus are allocated in that
+#' round itself. If nobody reaches quota then candidate with minimum number of votes
+#' is eliminated. If multiple candidates tie for minimum number of votes, one of
+#' them is selected at random and eliminated. The process is repeated till all
+#' of the seats are filled or number of candidates still in race equals number
+#' of unfilled seats. In later case, all of the active candidates are elected.
+#' Note: a ballot stays active till either it runs out of marked choices or gets
+#' removed during surplus allocation.
+#'
+#' @param x a data.frame with rows as ballots and columns as candidates. \code{x}
+#'     must pass all checks from \code{validateBallots()}.
+#' @param seats a number indicating candidates to elect. (default = 1)
+#' @param file a character string naming file. "" indicates output to the console only (default).
+#'     Saves a CSV file. So, name should contain ".csv" at the end.
+#' @param surplusMethod a character string indicating which method to use for
+#'     surplus allocation. Currently supports only one option "Cambridge" (default)
+#' @param quotaMethod a character string indicating which method to use for
+#'     calculation of quota. Currently support only one option "Droop" (default)
+#'
+#' @return a data.frame with rows containing detailed results from each round of STV counting.
+#'     For any given round of counting, a row contains: number of active ballots, seats to fill,
+#'     quota, maximum and minimum votes obtained by any candidate, who was eliminated (if any),
+#'     if there was a tie for elimination (if yes, how many tied), who was elected (if any),
+#'     surplus if elected and each candidate's votes tally for that round.
+#'
+#' @examples
+#' stv(voting.data, seats = 4, file = "voting_results.csv")
+
+stv <- function(x, seats = 1, file = "", surplusMethod = "Cambridge", quotaMethod = "Droop") {
+
+  # Ensure supported surpluse and quota methods are selected
+  if(surplusMethod != "Cambridge") stop("Please set surplusMethod = 'Cambridge'. This is currently the only supported method.")
+  if(quotaMethod != "Droop") stop("Please set quotaMethod = 'Droop'. This is currently the only supported method.")
+
+  junk <- validateBallots(x)
+
+  if(seats > ncol(x)) stop("Number of seats must be less than or equal to the number of candidates.")
+
+  # Initialize various things:
+  elim <- c()                                  # Store names of eliminated candidates
+  elect <- c()                                 # Store names of elected candidates
+  included.ballots <- rep(TRUE, nrow(x))       # Ballots included at a given round
+  unfilled <- seats                            # Vacant seats at a given round
+  Nround <- 0                                  # Current round number
+
+  res <- data.frame(matrix(NA, ncol = 9 + ncol(x), nrow = 0)) # rename as res
+  names(res) <- c("ballots", "seats.to.fill", "quota", "max.vote.count", "min.vote.count",
+                  "elim.cand", "tied.for.elim", "elect.cand", "surplus", names(x))
+
+  # Iteratively elect/eliminate candidates till all seats are filled:
+  while (unfilled > 0) {
+    Nround <- Nround + 1
+    res[Nround, ] <- rep(NA, ncol(res))
+    res$seats.to.fill[Nround] <- unfilled
+    curr.candidates <- setdiff(names(x), c(elim, elect))
+
+    # Check if #remaining candidates already equal #unfilled seats:
+    if (unfilled == length(curr.candidates)) {
+      elect <- c(elect, curr.candidates)
+      res$elect.cand[Nround] <- paste(curr.candidates, collapse = "; ")
+      res$seats.to.fill[Nround] <- 0
+      res[Nround, elect] <- "Elected"
+      res[Nround, elim] <- "Eliminated"
+      res$quota[Nround] <- "Won by elim"
+
+      if (file != "") {
+        if (substr(file, nchar(file)-3, nchar(file)) != ".csv") warning("File name provided does not include .csv extention")
+        write.table(res, file = file, sep = ",", row.names = FALSE)
+      }
+
+      return(res)
+      # return(list("elected" = elect, "detailed.info" = res))
+    }
+
+    # For remaining candidates, get valid ballots (1. not empty 2. not excluded in prior round)
+    curr.ballots <- (rowSums(!is.na(x[ ,curr.candidates])) > 0) & included.ballots
+    ballot.size <- sum(curr.ballots)
+    res$ballots[Nround] <- ballot.size
+
+    # Calculate Quota: Manually add 1 instead of using "ceiling()" to address whole numbers
+    if (quotaMethod == "Droop") quota <- floor(ballot.size/(unfilled + 1)) + 1
+    res$quota[Nround] <- quota
+
+    # Get top choice for each valid ballot then tabulate it (i.e. get vote count for each candidate):
+    top.choice <- apply(x[ ,curr.candidates], 1, function(i.row) names(x[ ,curr.candidates])[which.min(i.row)]) # try using curr.candidates instead of names(x[ ,curr.cand])
+    top.choice[!curr.ballots] <- NA
+    vote.counts <- table(factor(top.choice, levels = curr.candidates))
+
+    res[Nround, names(vote.counts)] <- vote.counts
+    res$max.vote.count[Nround] <- max(vote.counts)
+    res$min.vote.count[Nround] <- min(vote.counts)
+    res[Nround, elect] <- "Elected"
+    res[Nround, elim] <- "Eliminated"
+
+    # Check for elected candidate(s): If quota crossed, surplus distribution by Cambridge method.
+    #   else: eliminate a candidate and redistribute votes
+    if (any(vote.counts >= quota)) {
+      curr.elected <- vote.counts[vote.counts >= quota]
+      elect <- c(elect, names(curr.elected))
+      unfilled <- seats - length(elect)
+      res$elect.cand[Nround] <- paste(names(curr.elected), collapse = "; ")
+      res$surplus[Nround] <- paste(curr.elected - quota, collapse = "; ")
+
+      # Redistribute surplus votes (Cambridge method only for now)
+      for (i in names(curr.elected)) {
+        cand.ballots <- which(top.choice == i)
+        if (surplusMethod == "Cambridge") {
+          included.ballots[sample(cand.ballots, quota)] <- FALSE
+        }
+      } # CLOSE surplus allocation
+    } else {
+      curr.elim <- names(which(vote.counts == min(vote.counts)))
+      if (length(curr.elim) > 1) res$tied.for.elim[Nround] <- paste("Yes: ", length(curr.elim), sep = "")
+      elim <- c(elim, sample(curr.elim, 1))
+      res$elim.cand[Nround] <- tail(elim, 1)
+    } # CLOSE elim/elect if-else function
+
+  } # CLOSE while-loop
+
+  if (file != "") {
+    if (substr(file, nchar(file)-3, nchar(file)) != ".csv") warning("File name provided does not include .csv extention")
+    write.table(res, file = file, sep = ",", row.names = FALSE)
+  }
+
+  return(res)
+  # return(list("elected" = elect, "detailed.info" = res))
+}
+
+
---
+++ R/validate_clean_ballots.R
@@ -0,0 +1,155 @@
+#' Validates Input Data Format for \code{stv()}.
+#'
+#' Validates data format for \code{stv()} input. Assumes each row and column
+#' corresponds to a ballot and candidate respectively. See details for the
+#' tests run by this function. If input data is in correct format, returns
+#' string: "All tests passed. Please feel free to run \code{stv()} function.".
+#' Otherwise, prints corresponding warning message.
+#'
+#' Checks if input data is in acceptable format for \code{stv()}. User must run
+#' this function before calling \code{stv()}. Before any calculation, \code{stv()}
+#' calls this function and proceeds only if \code{x} passes all of the following tests
+#' (run in given order).
+#' \enumerate{
+#'   \item If \code{x} is a \code{data.frame}.
+#'   \item If \code{x} has unique column names.
+#'   \item If \code{x} has numeric entries.
+#'   \item If \code{x} doesn't have any blank column.
+#'   \item If \code{x} doesn't have any blank row or a row with non-sequential ranks.
+#' }
+#'
+#' @param x a \code{data.frame} with rows as ballots and columns as candidates.
+#'
+#' @return string: "All tests passed. Please feel free to run stv() function." if \code{x}
+#'   passes all tests. Otherwise, corresponding warning message. NOTE: Some of the
+#'   warnings can be fixed using \code{cleanBallots()} function. Other issues must be
+#'   fixed by the user.
+#'
+#' @examples
+#' validateBallots(voting.data)
+
+validateBallots <- function(x) {
+
+  # 1. Allowed Class: data.frame
+  if (class(x) != "data.frame") stop("\nPlease enter object of class data.frame, or use cleanBallots().")
+
+  # 2. Check sanity of column names:
+  if (any(is.na(names(x)))) stop("\nPlease provide each candidate's name/identifier as column names.")
+  if (length(unique(names(x))) != ncol(x)) stop("\nPlease provide unique column names.")
+
+  # 3. Check if x is numeric:
+  if (any(!sapply(x, is.numeric))) {
+    print(paste("Column(s):",
+                paste(which(!sapply(x, is.numeric)), collapse = ", "),
+                "contain non-numeric data."))
+    stop("\nPlease provide numeric input.")
+  }
+
+  # 4. Check for blank cols:
+  if (any(colSums(is.na(x)) == nrow(x))) {
+    print(paste("Column(s):",
+                paste(which(colSums(is.na(x)) == nrow(x)), collapse = ","),
+                "do not contain any ranks."))
+    stop("\nPlease remove column(s) for candidate(s) not ranked, or use cleanBallots()")
+  }
+
+  # 5. Check for blank rows and rows w/ non-sequencial ranks
+  if (any(rowSums(is.na(x)) == ncol(x))) {
+    print(paste("Row(s):",
+                paste(which(rowSums(is.na(x)) == ncol(x)), collapse = ", "),
+                "do not contain any ranks."))
+    stop("\nPlease remove blank row(s), or use cleanBallots()")
+  }
+
+  valid <- rep(FALSE, nrow(x))
+  for (i in 1:nrow(x)) {
+    if (identical(as.numeric(sort(x[i,])), as.numeric(1:max(x[i,], na.rm = TRUE)))) {
+      valid[i] <- TRUE
+    }
+  }
+
+  if (any(!valid)) {
+    print(paste("Row(s):",
+                paste(which(!valid), collapse = ", "),
+                "contain non-sequencial (missing or duplicated) ranks."))
+    stop("\nPlease remove row(s) with non-sequencial ranks, or use cleanBallots()")
+  }
+
+  return("All tests passed. Please feel free to run stv() function.")
+}
+
+#' Tries to Address \code{validateBallots()} Warning(s).
+#'
+#' Tries to clean data for \code{stv()}. Some of the warnings from \code{validateBallots()}
+#' have to addressed by the user (see Details).
+#'
+#' Assumes \code{x} contains rows and columns corresponding to ballots and
+#' candidates respectively. Tries to address issues raised by \code{validateBallots()}
+#' in the following order:
+#' \enumerate{
+#'   \item If \code{x} is a \code{matrix} then converts to \code{data.frame}.
+#'     Otherwise, user has to convert \code{x} into \code{data.frame}.
+#'   \item Checks if \code{x} has numeric entries. If not, checks if numeric
+#'     data was passed as character. If this also fails, then user has to
+#'     convert data into numeric type.
+#'   \item If column names of \code{x} are missing assigns \code{cand.names} as
+#'     column names. If
+#'   \code{x} already has valid column names, no need to specify \code{cand.names}.
+#'   If column names of \code{x} missing and \code{cand.names} not specified, returns
+#'   error message.
+#'   \item Removes blank columns.
+#'   \item Removes blank and/or non-sequentially ranked rows.
+#' }
+#'
+#' @param x a \code{data.frame} with rows as ballots and columns as candidates.
+#' @param cand.names character vector of length equal to number of candidates
+#'   (needed when column names of \code{x} are missing or not unique).
+#'
+#' @return a \code{data.frame} compatible for \code{stv()} function.
+#'
+#' @examples
+#' cleanBallots(voting.data)
+
+cleanBallots <- function(x, cand.names = NA) {
+
+  # 1. Check if input: matrix or data.frame, convert matrix into data.frame
+  if (!(class(x) %in% c("data.frame", "matrix"))) stop("\nPlease enter object of class either data frame or matrix.")
+  if (class(x) == "matrix") x <- as.data.frame(x, stringsAsFactors = FALSE)
+
+  # 2. Check if x is numeric:
+  cols.non.numeric <- !sapply(x, is.numeric)
+  if (any(cols.non.numeric)) {
+    for (i in which(cols.non.numeric)) {
+      if (is.factor(x[,i])) x[,i] <- as.character(x[,i])
+      temp <- x[!is.na(x[,i]), i]
+      if (anyNA(suppressWarnings(as.numeric(temp)))) {
+        stop(paste("\nPlease check data type in column", i))
+      }
+      x[,i] <- as.numeric(x[,i])
+    }
+  }
+
+  # 3. Provide column names:
+  #--- CHECK ---: If x already has valid column names, is this code
+  #-------------: going to remove those names?
+  if (!is.na(cand.names)) {
+    if (length(cand.names) != ncol(x)) stop ("Please provide exactly one candidate name for each column.")
+    names(x) <- cand.names
+  }
+
+  # 4. Remove blank cols:
+  x <- x[, colSums(!is.na(x)) > 0]
+
+  # 5. Remove blank and/or non-sequentially ranked rows:
+  x <- x[rowSums(is.na(x)) != ncol(x), ]
+
+  valid <- rep(FALSE, nrow(x))
+  for(i in 1:nrow(x)) {
+    valid[i] <- identical(as.numeric(sort(x[i,])), as.numeric(1:max(x[i,], na.rm = TRUE)))
+  }
+  x <- x[valid, ]
+
+  if (class(try(validateBallots(x))) == "try-error") warning("Validation failed the validateBallots() check")
+  return(x)
+}
+
